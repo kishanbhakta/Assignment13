a5787549a9ece663a0a96855103bded4dc87cfd6
kishan bhakta <kishan.d.bhakta@gmail.com>
2015-06-03 01:34:49 -0400

    finished styling except couldn't get the proper distance between text and icons

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 console.log('This is cool!');
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
 
 $.getJSON("http://private-44f6d-timeline6.apiary-mock.com/timeline").done(function(items) {
 
-    console.log('data', items);
+    // console.log('data', items);
 
-    $('.container').html(template.ipsum(items));
+    items.timelines.forEach (function(timeline) {
+
+      var m = moment( new Date (timeline.timestamp));
+      timeline.date =  m.format("DD/MM/YY");
+      timeline.times = m.format("hh:mm");
+
+      // console.log(data)
+
+     return timeline;
+
+
+
+    });
+
+$('.container').html(template.kishan(items));
 
 });
 
 
+      // var title = data.title
+      // var content = data.content
+      // var icon = data.icon
+      // var timeStamp = data.timestamp;
